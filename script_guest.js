3f09fec5918f6af2af203bed6e0587db63f84e63
Joy Mukherjee <34181957+joymkj@users.noreply.github.com>
2023-07-10 05:45:30 -0400

    add guest and host pages

---
+++ script_guest.js
@@ -0,0 +1,137 @@
+// Get the microphone button element
+const microphoneButton = document.getElementById('microphoneButton');
+const utterance = document.getElementById('userUtterance');
+const userInput = document.getElementById('userInput');
+
+// Check if the browser supports the Web Speech API
+if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+  // Create an instance of the SpeechRecognition object
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+  // Set the recognition language
+  recognition.lang = 'en-US'; // Specify the desired language
+
+  // Add a click event listener to the button
+  microphoneButton.addEventListener('click', () => {
+    // Start speech recognition when the user speaks
+    recognition.start();
+    microphoneButton.classList.toggle('clicked');
+  });
+
+  // Define the event handler for when speech is recognized
+  recognition.onresult = (event) => {
+    const transcript = event.results[0][0].transcript;
+    console.log('Transcript:', transcript);
+    utterance.innerHTML = transcript;
+    userInput.value =
+      transcript +
+      '\n' +
+      "Answer the above question by imagining you are Alea, a chatbot for AirBnB which helps guests and hosts. The user's name is Joy and he is a guest. Reply precisely. Use these info if you don't know the answer: Wifi password - ALEA_123, airbnb location - 300 South Craig street, Pittsburgh 15217 ";
+    // Here, you can perform any desired actions with the transcribed text
+    // For example, you can update a text field or send the transcript to a server
+    event.preventDefault();
+
+    const url = `https://${API_ENDPOINT}/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict`;
+    console.log('API Endpoint: ', url);
+
+    const body = {
+      instances: [{ content: userInput.value }],
+      parameters: {
+        temperature: 0.1,
+        maxOutputTokens: 256,
+        topP: 0.46,
+        topK: 16,
+      },
+    };
+
+    console.log('Request Body: ', body);
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${AUTH_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log('API Response: ', response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log('API Data: ', data);
+        if (data.predictions && data.predictions.length > 0 && data.predictions[0].content) {
+          outputPara.textContent = `Output: ${data.predictions[0].content}`;
+        } else {
+          console.log('Invalid or empty predictions in API data: ', data);
+          outputPara.textContent = `No output received from API`;
+        }
+      })
+      .catch((error) => {
+        console.log('API Error: ', error);
+        outputPara.textContent = `Error: ${error}`;
+      });
+  };
+} else {
+  console.log('Speech recognition not supported in this browser.');
+}
+const API_ENDPOINT = 'us-central1-aiplatform.googleapis.com';
+const PROJECT_ID = 'grand-kingdom-392214';
+const MODEL_ID = 'text-bison@001';
+let AUTH_TOKEN =
+  'ya29.a0AbVbY6N1bPJzD_bF9dI8gQBJjpAfU5cxnVUr-nwYYe8wj-o1rDe1lWUIlMowXz8jv3Yt66vco3MdUP_sEv0XHZubdTftDfF3rhwDDuxVFmpKMKQVZtNInqzuqMS-KV5ctxz_uxma6KL4NOFw7Xa0MUnB4Tr-XXNEh581aPi59alHAIdEBF5WhS1GO7gzVMjRbAJ_IHXJx9VuV4qrZY-Vqx7mS52cEzCo6gvSNdOUcjGGmck8dJPPuItvdJlaZeJAJYGfCaxFiYsIErjWdWm6SHGjz6Ld9m5l_njXHcwHeTJpHVlRQ4PgVcnSxlBVczIwkHnvwFNEwfMFdJryxicjIC76Dqr3JHd2lAR_aoMCltxLOAtZXdKLoIDZJnZkL4ZMKY8MxzGL5v7n50r-07mCOce2sgaCgYKAccSARMSFQFWKvPlWeAgAwtDLW472N2bbPfIfA0417';
+const form = document.getElementById('apiForm');
+const outputPara = document.getElementById('output');
+// AUTH_TOKEN = AUTH_TOKEN.replace('awdOFE6wFg', '');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const userInput = document.getElementById('userInput').value;
+  let contexeduserInput =
+    userInput +
+    '\n' +
+    "Answer the above question by imagining you are Alea, a chatbot for AirBnB which helps guests and hosts. The user's name is Joy and he is a guest. Reply precisely. Use these info if you don't know the answer: Wifi password - ALEA_123, airbnb location - 300 South Craig street, Pittsburgh 15217 ";
+  console.log('User Input: ', contexeduserInput);
+
+  const url = `https://${API_ENDPOINT}/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:predict`;
+  console.log('API Endpoint: ', url);
+
+  const body = {
+    instances: [{ content: contexeduserInput }],
+    parameters: {
+      temperature: 0.1,
+      maxOutputTokens: 256,
+      topP: 0.46,
+      topK: 16,
+    },
+  };
+
+  console.log('Request Body: ', body);
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${AUTH_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => {
+      console.log('API Response: ', response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log('API Data: ', data);
+      if (data.predictions && data.predictions.length > 0 && data.predictions[0].content) {
+        outputPara.value = `Alea: ${data.predictions[0].content}`;
+      } else {
+        console.log('Invalid or empty predictions in API data: ', data);
+        outputPara.value = `No output received from API`;
+      }
+    })
+    .catch((error) => {
+      console.log('API Error: ', error);
+      outputPara.textContent = `Error: ${error}`;
+    });
+});
